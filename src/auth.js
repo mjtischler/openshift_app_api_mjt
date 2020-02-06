fd7c2813795deb0b926c5e3f4c69734577570a3e
Matt Tischler <matt.tischler@perficient.com>
2020-02-06 11:34:44 -0500

    Updating dockerfile and auth scheme

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,5 @@
-// This should be stored more securely.
+// This is a poor way to handle secure values. Store user data in a DB, and secrets elsewhere.
+// Using process.env here is a way to test automatic Docker builds from a parallel repo.
 
 const auth = {
   socketTokenSecret: process.env.SOCKET_TOKEN_SECRET || '' // Add socket secret token here
