296105b9634f8a70e1e1385df1dc3cfc51156133
Matt Tischler <matt.tischler@perficient.com>
2020-02-07 13:52:11 -0500

    Updating logging

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import jwt from 'jsonwebtoken';
 import getAuthValues from './auth.js';
 
 const environment = process.env.NODE_ENV || 'production';
-const port = 8081;
-const socketIp = environment === 'production' ? '0.0.0.0' : null;
+const port = process.env.COMPONENT_BACKEND_PORT || 8081;
+const socketIp = process.env.COMPONENT_BACKEND_HOST || null;
 const ioServer = server.createServer()
 const ioSocket = io(ioServer);
 const sequenceNumberByClient = new Map();
@@ -16,22 +16,25 @@ ioSocket.use((socket, next) => {
   if (socket.handshake.query && socket.handshake.query.token){
     jwt.verify(socket.handshake.query.token, auth.socketTokenSecret, (err, decoded) => {
       if (err) {
+        console.log(`${err}`);
         return next(new Error('Authentication error'));
       }
       socket.decoded = decoded;
       next();
     });
   } else {
+    console.log('Other authentication error');
     next(new Error('Authentication error'));
   }    
 }).on('connection', (socket) => {
-    console.log(`Client connected on socket [id=${socket.id}]`);
+    const { address, time } = socket.handshake;
+    console.log(`Client connected on socket [id=${socket.id}] [ip=${address}] [time=${time}]`);
     sequenceNumberByClient.set(socket, 1);
 
     socket.on('clientEvent', (userId, callback) => {
       callback('Data Received from Socket');
 
-      console.log(`Received request from userId ${userId} on socket ${socket.id}`);
+      console.log(`Received request [userId=${userId}] [socket=${socket.id}]`);
       socket.emit('serverResponse', 'Data Received from Socket');
     });
 
@@ -41,6 +44,6 @@ ioSocket.use((socket, next) => {
     });
 });
 
-ioServer.listen(port, socketIp, () => console.log(`Socket.io listening on port ${port} in the ${environment} environment`));
+ioServer.listen(port, socketIp, () => console.log(`Socket.io listening [port=${port}] [environment=${environment}]`));
 
 export default ioServer;
